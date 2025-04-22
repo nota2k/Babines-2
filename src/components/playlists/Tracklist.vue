16bb63cb7cad9b489b162f7174dcfc1873183d14
Nelly <nelly.babillon@gmail.com>
2025-04-22 11:07:06 +0200

    feat: ajouter un composant pour ajouter des morceaux manuellement et améliorer le rendu des éléments dans la liste des morceaux

--- src/components/playlists/Tracklist.vue
+++ src/components/playlists/Tracklist.vue
@@ -2,6 +2,7 @@
 import { ref, onMounted, defineProps, onBeforeMount, onUpdated, watch } from 'vue'
 import { useCouchDBStore } from '@/stores/couchdb'
 import { useRoute } from 'vue-router'
+import AddTrackManually from './addTrackManually.vue'
 
 const route = useRoute()
 const store = useCouchDBStore();
@@ -77,9 +78,9 @@ const sortTracksByAdded = (event) => {
       <AddTrackManually />
       <h2 class="playlist-name">
         {{
-          store.currentPlaylist?.name
-            ? store.currentPlaylist.name
-            : 'Tous mes morceaux'
+        store.currentPlaylist?.name
+        ? store.currentPlaylist.name
+        : 'Tous mes morceaux'
         }}
       </h2>
       <table class="">
@@ -114,10 +115,7 @@ const sortTracksByAdded = (event) => {
         </thead>
 
         <tbody>
-          <tr
-            v-for="track in tracks "
-            :key="track._id"
-          >
+          <tr v-for="track in tracks " :key="track._id">
             <td class="title">
               {{ track.title }}
             </td>
@@ -141,23 +139,13 @@ const sortTracksByAdded = (event) => {
                 }"
                 class="yt"
               > -->
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  class="lucide lucide-youtube"
-                >
-                  <path
-                    d="M2.5 17a24.12 24.12 0 0 1 0-10 2 2 0 0 1 1.4-1.4 49.56 49.56 0 0 1 16.2 0A2 2 0 0 1 21.5 7a24.12 24.12 0 0 1 0 10 2 2 0 0 1-1.4 1.4 49.55 49.55 0 0 1-16.2 0A2 2 0 0 1 2.5 17"
-                  />
-                  <path d="m10 15 5-3-5-3z" />
-                </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                class="lucide lucide-youtube">
+                <path
+                  d="M2.5 17a24.12 24.12 0 0 1 0-10 2 2 0 0 1 1.4-1.4 49.56 49.56 0 0 1 16.2 0A2 2 0 0 1 21.5 7a24.12 24.12 0 0 1 0 10 2 2 0 0 1-1.4 1.4 49.55 49.55 0 0 1-16.2 0A2 2 0 0 1 2.5 17" />
+                <path d="m10 15 5-3-5-3z" />
+              </svg>
               <!-- </router-link> -->
             </td>
           </tr>
